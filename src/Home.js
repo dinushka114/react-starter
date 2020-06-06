4bdaf91e056f1240262a7fce08526abdb100602c
dinushka <dinushka piyumal>
2020-06-06 08:59:39 +0530

    first commit

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import  React  from 'react';
+import ReactDom from 'react-dom';
+import './home.css';
+
+function Home(){
+    return(
+        <h1>Hello React</h1>
+    )
+}
+
+export default Home;
